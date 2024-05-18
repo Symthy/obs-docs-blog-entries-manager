af76f15b25fae76d8e910280c6d975ec979ede13
Symthy <sympathia3104@gmail.com>
2024-05-18 18:29:25 +0900

    add: can push summary entry after updated entry pickup

--- docs_and_blog_entries_manager/application/service/entry_pickup_updater_service.py
+++ docs_and_blog_entries_manager/application/service/entry_pickup_updater_service.py
@@ -1,16 +1,21 @@
 from typing import List
 
+from application.service.summary_entry_pusher_service import SummaryEntryPusherService
 from domain.docs.value.doc_entry_id import DocEntryId
 from infrastructure.store.composite.stored_both_entries_accessor import StoredBothEntriesAccessor
 
 
 class EntryPickupUpdaterService:
-    def __init__(self, stored_entries_accessor: StoredBothEntriesAccessor):
+    def __init__(self, stored_entries_accessor: StoredBothEntriesAccessor,
+                 summary_entry_pusher: SummaryEntryPusherService):
         self.__stored_entries_accessor = stored_entries_accessor
+        self.__summary_entry_pusher = summary_entry_pusher
 
     def update_bulk(self, doc_entry_ids: List[DocEntryId], pickup: bool):
         for doc_entry_id in doc_entry_ids:
-            self.update(doc_entry_id, pickup)
+            self.__stored_entries_accessor.update_pickup(doc_entry_id, pickup)
+        self.__summary_entry_pusher.push_bulk()
 
     def update(self, doc_id: DocEntryId, pickup: bool):
         self.__stored_entries_accessor.update_pickup(doc_id, pickup)
+        self.__summary_entry_pusher.push_bulk()
--- docs_and_blog_entries_manager/application/service/summary_entry_pusher_service.py
+++ docs_and_blog_entries_manager/application/service/summary_entry_pusher_service.py
@@ -6,18 +6,23 @@ from infrastructure.documents.document_file_accessor import DocumentFileAccessor
 from infrastructure.hatena.blog_entry_repository import BlogEntryRepository
 
 
-class SummaryPusherService:
+class SummaryEntryPusherService:
     def __init__(self, entries_summary_factory: EntriesSummaryFactory, blog_entry_repository: BlogEntryRepository,
-                 document_file_accessor: DocumentFileAccessor):
+                 document_file_accessor: DocumentFileAccessor, summary_entry_id: BlogEntryId):
         self.__entries_summary_factory = entries_summary_factory
         self.__blog_entry_repository = blog_entry_repository
         self.__document_file_accessor = document_file_accessor
+        self.__summary_entry_id = summary_entry_id
+
+    def push_bulk(self):
+        self.push_doc_summary()
+        self.push_blog_summary()
 
     def push_doc_summary(self):
         summary = self.__entries_summary_factory.build()
         self.__document_file_accessor.save_summary_file(summary.content)
 
-    def push_blog_summary(self, summary_entry_id: BlogEntryId):
+    def push_blog_summary(self):
         summary = self.__entries_summary_factory.build()
         summary_entry = PostBlogEntry(summary.title, summary.list_text, CategoryPath('Summary'), [])
-        self.__blog_entry_repository.put_summary_page(summary_entry_id, summary_entry)
+        self.__blog_entry_repository.put_summary_page(self.__summary_entry_id, summary_entry)
