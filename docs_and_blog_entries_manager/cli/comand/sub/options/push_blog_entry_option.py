b4a5b4db0bf5da3ee989932d1c0e96b3907a6199
Symthy <sympathia3104@gmail.com>
2024-05-25 21:48:22 +0900

    add: push blog entry command option

---
+++ docs_and_blog_entries_manager/cli/comand/sub/options/push_blog_entry_option.py
@@ -0,0 +1,20 @@
+import argparse
+
+from application.service.blog_entry_pusher_service import BlogEntryPusherService
+from cli.comand.interface import ISubCommandOption
+from domain.docs.value.doc_entry_id import DocEntryId
+
+
+class PushBlogEntryOption(ISubCommandOption):
+    def __init__(self, blog_entry_pusher_service: BlogEntryPusherService):
+        self.__blog_entry_pusher_service = blog_entry_pusher_service
+
+    def add_option(self, subparser: argparse.ArgumentParser):
+        subparser.add_argument('--push', action='store', nargs=1, type=str, required=True)
+
+    def equals(self, args):
+        return args.push
+
+    def execute(self, args):
+        doc_entry_id = DocEntryId(args.push)
+        self.__blog_entry_pusher_service.execute(doc_entry_id)
