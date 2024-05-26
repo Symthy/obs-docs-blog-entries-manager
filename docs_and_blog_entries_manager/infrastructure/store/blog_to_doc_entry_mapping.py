6be47bf5991ca2d1193180e86598236ae0188a8e
Symthy <sympathia3104@gmail.com>
2024-05-26 10:43:25 +0900

    test: blog to doc entry mapping

--- docs_and_blog_entries_manager/infrastructure/store/blog_to_doc_entry_mapping.py
+++ docs_and_blog_entries_manager/infrastructure/store/blog_to_doc_entry_mapping.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional, List
+from typing import Dict, Optional
 
 from common.constants import LOCAL_STORAGE_DIR_PATH
 from domain.blogs.value.blog_entry_id import BlogEntryId
@@ -14,7 +14,7 @@ class BlogToDocEntryMapping:
         blog_to_docs: Dict[str, str] = json_file.load(
             self.__HATENA_BLOG_TO_DOC_ENTRY_PATH if stored_json_file_path is None else stored_json_file_path)
         self.__blog_id_to_doc_id: Dict[BlogEntryId, DocEntryId] = {BlogEntryId(b): DocEntryId(d) for b, d in
-                                                                   blog_to_docs}
+                                                                   blog_to_docs.items()}
         self.__doc_id_to_blog_id: Dict[DocEntryId, BlogEntryId] = {}
         for blog_entry_id, doc_entry_id in self.__blog_id_to_doc_id.items():
             self.__doc_id_to_blog_id[doc_entry_id] = blog_entry_id
@@ -24,7 +24,7 @@ class BlogToDocEntryMapping:
             return self.__doc_id_to_blog_id[doc_entry_id]
         return None
 
-    def find_blog_entry_ids(self, doc_entry_ids: List[DocEntryId]) -> List[BlogEntryId]:
+    def find_blog_entry_ids(self, doc_entry_ids: list[DocEntryId]) -> list[BlogEntryId]:
         blog_entry_ids = []
         for doc_entry_id in doc_entry_ids:
             blog_entry_id_opt = self.find_blog_entry_id(doc_entry_id)
@@ -34,7 +34,7 @@ class BlogToDocEntryMapping:
         return blog_entry_ids
 
     def find_doc_entry_id(self, blog_entry_id: BlogEntryId) -> Optional[DocEntryId]:
-        if blog_entry_id in self.__blog_id_to_doc_id:
+        if self.exist(blog_entry_id):
             return self.__blog_id_to_doc_id[blog_entry_id]
         return None
 
